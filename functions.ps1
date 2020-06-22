66ee993080b3b01f0a18acbf35606bebefc67358
James Cohen <xrsxj00@gmail.com>
2020-06-21 21:53:08 -0700

    update 1

--- functions.ps1
+++ functions.ps1
@@ -1,180 +1,23 @@
-function SearchResult($searchitem) {
-    foreach-object { 
-        foreach ($property in $_.PSObject.Properties) {
-            if ($property.value -like "*$($searchitem)*") {
-                $result = $_
-            }
-        }
-    }
-    return $result
-}
-
-function WritePasswords() {
-    #$body.asset_password = $body.asset_password | Where-Object { $null -ne $_.Value -and $_.Value -ne 'null' }
-    if ($oldpasswords.count -gt 1) {
-        foreach ($oldpassword in $oldpasswords) {
-            try {
-                (Invoke-Restmethod -Uri "$($huduurl)/asset_passwords/$($oldpassword.id)" -Method DELETE -Headers $huduheads).data                           
-            }
-            catch {
-                $_.Exception.Message
-                "$($huduurl)/asset_passwords/$($oldpassword.id)"
-            }
-        }
-        try {
-            Write-Host "Re-Creating $($item.name)"
-            (Invoke-Restmethod -Uri "$($huduurl)/asset_passwords" -Method POST -Headers $huduheads -Body $($body | ConvertTo-Json -depth 6)).data
-        }
-        catch {
-            $_.Exception.Message
-            "$($huduurl)/companies/asset_passwords"
-            $body
-        }
-    }
-    elseif ($oldpasswords) {
-        try {
-            Write-Host "Updating $($item.name)"
-            (Invoke-Restmethod -Uri "$($huduurl)/asset_passwords/$($oldpasswords.id)" -Method PUT -Headers $huduheads -Body $($body | ConvertTo-Json -depth 6)).data
-        }
-        catch {
-            $_.Exception.Message
-            "$($huduurl)/asset_passwords/$($oldpasswords.id)"
-            $body
-        }
-    }
-    else {
-        try {
-            Write-Host "Creating $($item.name)"
-            (Invoke-Restmethod -Uri "$($huduurl)/asset_passwords" -Method POST -Headers $huduheads -Body $($body | ConvertTo-Json -depth 6)).data
-        }
-        catch {
-            $_.Exception.Message
-            "$($huduurl)/asset_passwords"
-            $body
-        }
-    }
-}
-function GetHUDUPasswords() {
-    $i = 1
-    $passwords = New-Object System.Collections.Generic.List[System.Object]
-    while ($i -lt 9999) {
-        try {
-            $newpasswords = (Invoke-Restmethod -Uri "$($huduurl)/asset_passwords?page=$i&page_size=500" -Headers $huduheads)
-            if ($null -ne $newpasswords -and $newpasswords.asset_passwords.count -eq 0) {
-                $newpasswords = $newpasswords | ConvertFrom-Json -AsHashTable
-            }
-        }
-        catch {
-            $newassets > $null
-        }
-        $passwords += $newpasswords.asset_passwords
-        if ($($newpasswords.asset_passwords).count -lt 500) {
-            break
-        }
-        $i++
-    }
-    return $passwords
-}
-
-#### FROM Unifi ########
-
-function GetSites() {
-    $Sites = (Invoke-Restmethod -Uri "$($controller)/api/self/sites" -WebSession $myWebSession).data
-    return $Sites
-}
-
-function UniFiLogin() {
-    try {
-        Invoke-Restmethod -Uri "$($controller)/api/login" -method post -body $credential -ContentType "application/json; charset=utf-8"  -SessionVariable myWebSession | Out-Null
-    }
-    catch {
-        Write-Host $_.Exception.Message
-    }
-    return $myWebSession
-}
-function GetPortForwards() {
-    $pforwards = (Invoke-Restmethod -Uri "$($controller)/api/s/$($site.name)/stat/portforward" -WebSession $myWebSession).data
-    $forwards = New-Object System.Collections.Generic.List[System.Object]
-
-    foreach ($pforward in $pforwards) {
-        $forwards += "<b>Rule</b>$($pforward.name) - $(if ($pforward.enabled -ne $true) {"<strike>"})$(if ($pforward.src) {"$($pforward.src):"})$($pforward.dst_port)/$($pforward.proto) to $($pforward.fwd):$($pforward.fwd_port)/$($pforward.proto) $(if ($pforward.enabled -ne $true) {"</strike>"})<br>"
-    }
-    return "$forwards"
-}
-
-function GetSpeedTest() {
-    $test = New-Object System.Collections.Generic.List[System.Object]
-    $end = [Math]::Floor(1000 * (Get-Date ([datetime]::UtcNow) -UFormat %s))
-    $start = [Math]::Floor($end - 86400000)
-    $body = @{
-        attrs = 'xput_download', 'xput_upload', 'latency', 'time'
-        start = $start
-        end   = $end
-    } | convertTo-Json
-    $test = (Invoke-Restmethod -Uri "$($controller)/api/s/$($site.name)/stat/report/archive.speedtest" -WebSession $myWebSession -Method POST -ContentType "application/json" -Body $body).data[0]
-    if ($test.xput_download -gt 1) {
-        $testdate = Get-Date (New-Object System.DateTime (1970, 1, 1, 0, 0, 0, [System.DateTimeKind]::Utc)).AddMilliseconds($test.time) -Format "MM-dd-yyyy HH:mm"
-        $stest = "DL $([math]::Round($test.xput_download,2)) Mbps / UL $([math]::Round($test.xput_upload,2)) Mbps<br>Latency $($test.latency) ms<br>Performed: $testdate"
-    }
-    else {
-        $stest = "No recent test available."
-    }
-    return "$stest"
-}
-
-function GetCompanies() {
-    $i = 1
-    $Companies = New-Object System.Collections.Generic.List[System.Object]
-    while ($i -lt 9999) {
-        $newcomps = ((Invoke-Restmethod -Uri "$($huduurl)/companies?page=$i&page_size=999" -Headers $huduheads).companies)
-        $i++
-        if ($newcomps.count -eq 0) {
-            break
-        }
-        $Companies += $newcomps
-    }
-    return $Companies
-}
-
-function GetAssets() {
-    $i = 1
-    $assets = New-Object System.Collections.Generic.List[System.Object]
-    while ($i -lt 9999) {
-        try {
-            $newassets = ((Invoke-WebRequest -Uri  "$($huduurl)/companies/$($company.id)/assets?page=$i&page_size=999" -Headers $huduheads) -creplace "CrashPlan", "CrashPlan2" | ConvertFrom-Json)
-        }
-        catch {
-            $newassets > $null
-        }
-        $assets += $newassets.assets
-        if ($($newassets.assets).count -lt 25) {
-            break
-        }
-        $i++
-    }
-    return $assets
-}
-
 function WriteAssets() {
-    $body.asset.fields = $body.asset.fields | Where-Object { $null -ne $_.value }
+    $body.asset.fields = $body.asset.fields | Where-Object { $null -ne $_.value -and $_.value -ne "" }
     if ($name -notmatch "element-") {
-        if ($oldassets.count -gt 1 -and $null -ne $oldassets[0].fields) {
+        if ($oldassets.count -gt 1 -and $oldassets[0].fields) {
             foreach ($oldasset in $oldassets) {
                 try {
-                    (Invoke-Restmethod -Uri "$($huduurl)/companies/$($company.id)/assets/$($oldasset.id)" -Method DELETE -Headers $huduheads).data                           
+                    (Invoke-Restmethod -Uri "$($huduurl)/companies/$huduid/assets/$($oldasset.id)" -Method DELETE -Headers $huduheads).data                           
                 }
                 catch {
                     $_.Exception.Message
-                    "$($huduurl)/companies/$($company.id)/assets/$($oldasset.id)"
+                    "$($huduurl)/companies/$huduid/assets/$($oldasset.id)"
                 }
             }
             try {
                 Write-Host "Re-Creating $name"
-                (Invoke-Restmethod -Uri "$($huduurl)/companies/$($company.id)/assets" -Method POST -Headers    $huduheads -Body $($body | ConvertTo-Json -depth 6)).data
+                (Invoke-Restmethod -Uri "$($huduurl)/companies/$huduid/assets" -Method POST -Headers    $huduheads -Body $($body | ConvertTo-Json -depth 6)).data
             }
             catch {
                 $_.Exception.Message
-                "$($huduurl)/companies/$($company.id)/assets"
+                "$($huduurl)/companies/$huduid/assets"
                 $body | ConvertTo-Json -Depth 5
             }
         }
@@ -212,11 +55,11 @@ function WriteAssets() {
             if ($body.asset.fields) {
                 try {
                     Write-Host "Updating $($company.name) $name"
-                    (Invoke-Restmethod -Uri "$($huduurl)/companies/$($company.id)/assets/$($oldassets.id)" -Method PUT -Headers  $huduheads -Body $($body | ConvertTo-Json -depth 6)).data
+                    (Invoke-Restmethod -Uri "$($huduurl)/companies/$huduid/assets/$($oldassets.id)" -Method PUT -Headers  $huduheads -Body $($body | ConvertTo-Json -depth 6)).data
                 }
                 catch {
                     $_.Exception.Message
-                    "$($huduurl)/companies/$($company.id)/assets/$($oldassets.id)"
+                    "$($huduurl)/companies/$huduid/assets/$($oldassets.id)"
                     $body | ConvertTo-Json -Depth 5
                 }
             }
@@ -225,18 +68,17 @@ function WriteAssets() {
             try {
                 Write-Host "Creating $name"
                 $body | Out-File -Append -FilePath "./updates.creates.log" -Encoding UTF8 
-                (Invoke-Restmethod -Uri "$($huduurl)/companies/$($company.id)/assets" -Method POST -Headers  $huduheads -Body $($body | ConvertTo-Json -depth 6)).data
+                (Invoke-Restmethod -Uri "$($huduurl)/companies/$huduid/assets" -Method POST -Headers  $huduheads -Body $($body | ConvertTo-Json -depth 6)).data
             }
             catch {
                 $_.Exception.Message
-                "$($huduurl)/companies/$($company.id)/assets"
+                "$($huduurl)/companies/$huduid/assets"
                 $body | ConvertTo-Json -Depth 5
             }
         }
     }
 }
 function GetAttachedAssets($netassets) {
-    $netassets = $netassets | Where-Object { $_.archived -ne "True" }
     if ($netassets) {
         $attachedassets = New-Object System.Collections.Generic.List[System.Object]
         foreach ($asset in $netassets) {
@@ -253,61 +95,6 @@ function GetAttachedAssets($netassets) {
     }
     return $attachedassets
 }
-
-function SetName() {
-    $name = $location + " - " + $device.name
-    return $name
-}
-function SetLocation() {
-    $location = ($site.desc -replace $company.name, "" -replace '([\)\(]| \- )', '').Trim()
-    if ($location.Length -lt 2 ) {
-        $location = $company.city
-    }
-    return $location
-}
-function ArchiveOldAssets() {
-    if ($archiveassets.count -ne 0) {
-        foreach ($archiveasset in $archiveassets) {
-            if ($null -ne $archiveasset.id) {
-                try {
-                    Write-Host "Archiving old asset $($archiveasset.name)"
-                    (Invoke-Restmethod -Uri "$($huduurl)/companies/$($company.id)/assets/$($archiveasset.id)/archive" -Method PUT -Headers $huduheads).data                           
-                }
-                catch {
-                    $_.Exception.Message
-                    "$($huduurl)/companies/$($company.id)/assets/$($archiveasset.id)/archive"
-                }
-            }
-        }
-    }
-    $archiveassets = $false
-}
-
-function CreateArchiveList() {
-    if ($oldassets.count -eq 0) {
-        $oldassets = @{
-            id = 999999999
-        }
-    }
-    foreach ($oldasset in $oldassets) {
-        if ($archiveassets -ne $false) {
-            $archiveassets = $archiveassets | Where-Object { $_.id -ne $oldasset.id }
-        }
-        else {
-            $archiveassets = $assets | Where-Object { $_.asset_layout_id -eq $templateid -and $_.fields.value -match $location -and $_.fields.value -match "Powershell Script" -and $_.id -ne $oldasset.id }
-        }
-    }
-    return $archiveassets
-}
-function GetTemplateId($templatename) {
-    [int]$templateid = ($Layouts | Where-Object { $_.name -eq $templatename }).id
-    return $templateid
-}
-function GetFieldId($fieldname) {
-    [int]$fieldid = (($Layouts | Where-Object { $_.id -eq $templateid }).fields | Where-Object { $_.label -eq $fieldname }).id
-    return $fieldid
-}
-
 function GetDomainStructure ([string]$dn, $level = 1) { 
     if ($level -eq 1) { "<b><u>$((Get-ADDomain).DNSRoot)</b></u><br>" } 
     Get-ADObject -filter 'ObjectClass -eq "organizationalUnit" -or ObjectClass -eq "Container"' -SearchBase $dn -SearchScope OneLevel |  
@@ -345,4 +132,21 @@ function GetSyncContacts() {
     }
     return $contacts
 }
-
+function GetAssets() {
+    $i = 1
+    $assets = New-Object System.Collections.Generic.List[System.Object]
+    while ($i -lt 9999) {
+        try {
+            $newassets = ((Invoke-WebRequest -UseBasicParsing -Uri "$($huduurl)/companies/$huduid/assets?page=$i&page_size=999" -Headers $huduheads) -creplace "CrashPlan", "CrashPlan2" | ConvertFrom-Json)
+        }
+        catch {
+            $newassets > $null
+        }
+        $assets += $newassets.assets
+        if ($($newassets.assets).count -lt 25) {
+            break
+        }
+        $i++
+    }
+    return $assets
+}
